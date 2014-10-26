7db29707c7bda3aadc5e8f3a0b70e480b77797db
peacemaker07(Yuji Chino) <au4iu6@bma.biglobe.ne.jp>
2014-10-27 00:45:30 +0900

    commit

---
+++ mysite/polls/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.db import models
+from polls.models import Poll
+from polls.models import Choice
+
+# admin.site.register(Poll)
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+
+
+admin.site.register(Poll, PollAdmin)
+
+#admin.site.register(Choice)
+
+# Register your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
